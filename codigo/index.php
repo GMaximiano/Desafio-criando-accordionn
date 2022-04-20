4245cf0c6fe4698b6899fa07bb2356af34f42e00
GMaximiano <gabrieltrujilo9@gmail.com>
2022-04-19 22:14:34 -0300

    cria pasta codigo, move arquivo para codigo

---
+++ codigo/index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+      <title>Meu site de aprendizado dio</title>
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"> </script>
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+      <link rel="stylesheet" href="/resources/demos/style.css">
+      <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+      <style type="text/css">
+        h1{color:blue
+        }
+        
+      </style>
+    </head>
+    <body>
+        <h1>Meu gato e suas descriçoes</h1>
+        <div id="accordion">
+          <h3>Jean</h3>
+          <div>
+            <p>Esse é um gato de tamanho medio/grande cor marraom e olhos azuil e bem vagabundo</p>
+          </div>
+          <h3>Alisson</h3>
+          <div>
+            <p>O alisson e um gato medio branco com pintas marrom e completamente maluco</p>
+          </div>
+          <h3>Mag</h3>
+          <div>
+            <p>Essa e uma gata preta com partes brancas, a mais linda e de longe a melhor</p>
+          </div>
+
+        </div>
+    </body>
+    <script>
+      $( "#accordion" ).accordion();
+    </script>
+    <script type="text/javascript">
+    $(document) .ready(function() {
+      alert("EBA!!!");
+    });
+    </script>       
+</html>
